156fa115922c84851a96890e0dfa7c3f01b09552
Clinton Selke <clinuxrulz@gmail.com>
2016-02-07 11:07:14 +1000

    made a good test case

--- test/Main.purs
+++ test/Main.purs
@@ -1,6 +1,32 @@
 module Test.Main where
 
+import Prelude
+import Control.Monad.Cont.Trans.StackSafe
+import Control.Monad.Rec.Class
+import Control.Monad.Eff
 import Control.Monad.Eff.Console
+import Control.Monad.Trans
+import Data.Tuple
+import Data.Either
 
+main :: Eff (console :: CONSOLE) Unit
 main = do
-  log "You should add some tests."
+  runContT
+    (tailRecM
+      (\(Tuple a n) -> do
+        if n `mod` 1000 == 0
+          then do
+            lift $ log $ "upto: " ++ (show n)
+            return unit
+          else return unit
+        if n <= 1000000
+          then return $ Left $ Tuple (a+n) (n+1)
+          else do
+            lift $ log "done."
+            return $ Right $ a
+      )
+      (Tuple 0 0)
+    )
+    pure
+  return unit
+
