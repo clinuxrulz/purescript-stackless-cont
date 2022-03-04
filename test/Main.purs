8244dc3423e70f198edd4dd691f33df16a3d1abe
boletales <30789006+boletales@users.noreply.github.com>
2022-03-05 04:53:33 +0900

    bumped version - bower is replaced with spago - Eff is replaced with Effect - tailrec uses Step instead of Either - MonadAsk is separated from MonadReader

--- test/Main.purs
+++ test/Main.purs
@@ -1,32 +1,31 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Cont.Trans.StackSafe
+import Control.Monad.Stackless.Cont.Trans
 import Control.Monad.Rec.Class
-import Control.Monad.Eff
-import Control.Monad.Eff.Console
-import Control.Monad.Trans
+import Effect
+import Effect.Console
+import Control.Monad.Trans.Class
 import Data.Tuple
-import Data.Either
 
-main :: Eff (console :: CONSOLE) Unit
+main :: Effect Unit
 main = do
-  runContT
+  _ <- runContT
     (tailRecM
       (\(Tuple a n) -> do
         if n `mod` 1000 == 0
           then do
-            lift $ log $ "upto: " ++ (show n)
-            return unit
-          else return unit
+            lift $ log $ "upto: " <> (show n)
+            pure unit
+          else pure unit
         if n <= 1000000
-          then return $ Left $ Tuple (a+n) (n+1)
+          then pure $ Loop $ Tuple (a+n) (n+1)
           else do
             lift $ log "done."
-            return $ Right $ a
+            pure $ Done $ a
       )
       (Tuple 0 0)
     )
     pure
-  return unit
+  pure unit
 
